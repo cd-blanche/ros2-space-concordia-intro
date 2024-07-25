06b443bf142f92fc414d1b00c730ee95bb1d0e6a
poewt ubuntu <79345528+cd-blanche@users.noreply.github.com>
2024-07-24 21:03:20 -0400

    Init C++ publisher node

---
+++ src/build_my_string/src/cpp_publisher.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "build_my_string/msg/message.hpp"
+
+using namespace std::chrono_literals;
+
+class MessagePublisher : public rclcpp::Node
+{
+  public:
+    MessagePublisher()
+    : Node('message_publisher'), count_(0)
+    {
+      this->declare_parameter("user_message", "Hello world!");
+      publisher_ = this->create_publisher<build_my_string::msg::Message>("topic", 10);
+      timer_ = this->create_wall_timer(
+        500ms, std::bind(&MessagePublisher::timer_callback, this)
+      );
+    }
+  private:
+    void timer_callback()
+    {
+      build_my_string::msg::Message user_message = this->get_parameter("user_message").as_string();
+      auto message = build_my_string::msg::Message();
+      message.message = user_message;
+      this->count_++;
+      RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.message << "' " << "[" << count_ << "]" );
+    }
+    rclcpp::Publisher<build_my_string::msg::Message>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    size_t count_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MessagePublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
+
+
