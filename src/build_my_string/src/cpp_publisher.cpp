c524274c960f07dcc240b7eda223b4f40e5ee3a8
poewt-vm <79345528+cd-blanche@users.noreply.github.com>
2024-07-25 02:49:07 +0000

    Add comments + add small adjustments

--- src/build_my_string/src/cpp_publisher.cpp
+++ src/build_my_string/src/cpp_publisher.cpp
@@ -14,24 +14,33 @@ class MessagePublisher : public rclcpp::Node
     MessagePublisher()
     : Node("message_publisher"), count_(0)
     {
+      // Initialize custom parameter
       this->declare_parameter("message", "Hello world!");
+      // Create publisher
       publisher_ = this->create_publisher<build_my_string::msg::Message>("topic", 10);
+      // Create repeating loop that calls timer_callback() every x ms
       timer_ = this->create_wall_timer(
-        500ms, std::bind(&MessagePublisher::timer_callback, this)
+        50ms, std::bind(&MessagePublisher::timer_callback, this)
       );
     }
   private:
+    // Function called on repeat by timer_
     void timer_callback()
     {
+      // Get paramter
       std::string message = this->get_parameter("message").as_string();
+      // Set message to be published
       auto user_msg = build_my_string::msg::Message();
       user_msg.message = message;
-      this->count_++;
+      // Publish message
       RCLCPP_INFO(this->get_logger(), "Publishing: %s. [%zu]", user_msg.message.c_str(), this->count_);
+      // Increase current count
+      this->count_++;
     }
     rclcpp::Publisher<build_my_string::msg::Message>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     size_t count_;
+    // todo Variables to keep track of build progress
 };
 
 int main(int argc, char ** argv)
