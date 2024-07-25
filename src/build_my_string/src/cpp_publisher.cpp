5e2d07851fe9d8bdbc93c12aeb12e4b2c09a3abf
poewt ubuntu <79345528+cd-blanche@users.noreply.github.com>
2024-07-25 19:44:11 -0400

    Finalize player animation layer and celebration animation

--- src/build_my_string/src/cpp_publisher.cpp
+++ src/build_my_string/src/cpp_publisher.cpp
@@ -17,10 +17,13 @@ class MessagePublisher : public rclcpp::Node
       base_width(20),
       base_height(40),
       playerPos(0),
-      playerDirection(RIGHT)
+      playerDirection(RIGHT),
+      curr_msg_pos(0),
+      loop(0),
+      eyes('o')
     {
       // Initialize custom parameters
-      this->declare_parameter("message", "Hello world!");
+      this->declare_parameter("message", "Hi!");
       this->declare_parameter("base_w", base_width);
       this->declare_parameter("base_h", base_height);
 
@@ -36,7 +39,7 @@ class MessagePublisher : public rclcpp::Node
 
       // Create repeating loop that calls timer_callback() every x ms
       timer_ = this->create_wall_timer(
-        100ms, std::bind(&MessagePublisher::timer_callback, this)
+        50ms, std::bind(&MessagePublisher::timer_callback, this)
       );
     }
 
@@ -52,21 +55,27 @@ class MessagePublisher : public rclcpp::Node
       // Check to see if param still matches cached string
       if (input_message != cached_inp_msg) {
         cached_inp_msg = input_message;
-        count_ = 0; // Reset build count
+        reset();
       }
       if (base_width != cached_base_width) {
         cached_base_width = base_width;
-        count_ = 0;
         base_layer = "";
         set_base_width();
+        reset();
       }
       if (base_height != cached_base_height) {
         cached_base_height = base_height;
-        count_ = 0;
+        reset();
       }
 
-      // Print output
-      print_output();
+      if (curr_msg_pos < input_message.length()) {        // Print output
+        print_output();
+      } else {
+        // celebrate
+        celebrate();
+      }
+      
+
 
       // Increase current count
       this->count_++;
@@ -92,12 +101,18 @@ class MessagePublisher : public rclcpp::Node
     int cached_base_height;
 
     // player layer
-    int playerPos;
+    std::string player_layer;
+    size_t playerPos;
     PlayerDirection playerDirection;
 
     // message input
     std::string input_message;
     std::string cached_inp_msg;
+    size_t curr_msg_pos;
+
+    // celebration
+    size_t loop;
+    char eyes;
 
     // todo Function to build output
     void print_output()
@@ -109,48 +124,53 @@ class MessagePublisher : public rclcpp::Node
       print_player();
 
       // Set message to be published
-      auto user_msg = build_my_string::msg::Message();
-      user_msg.message = input_message;
+      // auto user_msg = build_my_string::msg::Message();
+      // user_msg.message = input_message;
 
       // Publish message
-      RCLCPP_INFO(this->get_logger(), "[cache: %s] Publishing: %s. [%zu]", cached_inp_msg.c_str(), user_msg.message.c_str(), this->count_);
+      // RCLCPP_INFO(this->get_logger(), "[cache: %s] Publishing: %s. [%zu]", cached_inp_msg.c_str(), user_msg.message.c_str(), this->count_);
     }
 
     // Function to print player layer
     void print_player()
     {
-      std::string player_layer = base_layer;
+      // Set new current layer
+      std::string curr_layer = player_layer;
 
       // Increment based on direction player is moving
       if (playerDirection == RIGHT) {
         playerPos++;
-        player_layer[playerPos] = '(';
-        player_layer[playerPos + 1] = '>';
-        player_layer[playerPos + 2] = 'o';
-        player_layer[playerPos + 3] = '_';
-        player_layer[playerPos + 4] = 'o';
-        player_layer[playerPos + 5] = ')';
-        player_layer[playerPos + 6] = '>';
-        // player_layer[playerPos + 7] = 'a';
+        curr_layer[playerPos] = '(';
+        curr_layer[playerPos + 1] = '>';
+        curr_layer[playerPos + 2] = 'o';
+        curr_layer[playerPos + 3] = '_';
+        curr_layer[playerPos + 4] = 'o';
+        curr_layer[playerPos + 5] = ')';
+        curr_layer[playerPos + 6] = '>';
+        // player holds the letter
+        curr_layer[playerPos + 7] = input_message[curr_msg_pos]; 
       } else if (playerDirection == LEFT) {
         playerPos--;
-        player_layer[playerPos] = '<';
-        player_layer[playerPos + 1] = '(';
-        player_layer[playerPos + 2] = 'o';
-        player_layer[playerPos + 3] = '_';
-        player_layer[playerPos + 4] = 'o';
-        player_layer[playerPos + 5] = '<';
-        player_layer[playerPos + 6] = ')';
+        curr_layer[playerPos] = '<';
+        curr_layer[playerPos + 1] = '(';
+        curr_layer[playerPos + 2] = 'o';
+        curr_layer[playerPos + 3] = '_';
+        curr_layer[playerPos + 4] = 'o';
+        curr_layer[playerPos + 5] = '<';
+        curr_layer[playerPos + 6] = ')';
       }
 
       // Once player reaches far end of the base, reverse direction
-      if (playerPos == base_width - 7) {
+      if (playerPos == player_layer.length() - 8) {
         playerDirection = LEFT;
+        // Append next character in message and continue with next in line
+        player_layer += input_message[curr_msg_pos]; 
+        curr_msg_pos++;
       } else if (playerPos == 0) {
         playerDirection = RIGHT;
       }
 
-      RCLCPP_INFO(this->get_logger(), player_layer.c_str());
+      RCLCPP_INFO(this->get_logger(), curr_layer.c_str());
     }
 
     // Function to print base layer
@@ -168,7 +188,69 @@ class MessagePublisher : public rclcpp::Node
       for (int i = 0; i < base_width; i++)
       {
         base_layer += ".";
+        player_layer = base_layer;
       }
+    }
+
+    // Function to celebrate after completing string
+    void celebrate()
+    {
+      print_base();
+      print_dance();
+    }
+
+    // Function to print dance animation
+    void print_dance()
+    {
+      // Set current animation layer
+      std::string dance_layer = player_layer;
+
+      // Increment based on direction player is moving
+      if (playerDirection == RIGHT) {
+        playerPos++;
+        dance_layer[playerPos] = '(';
+        dance_layer[playerPos + 1] = '\\';
+        dance_layer[playerPos + 2] = eyes;
+        dance_layer[playerPos + 3] = '_';
+        dance_layer[playerPos + 4] = eyes;
+        dance_layer[playerPos + 5] = ')';
+        dance_layer[playerPos + 6] = '/';
+      } else if (playerDirection == LEFT) {
+        playerPos--;
+        dance_layer[playerPos] = '\\';
+        dance_layer[playerPos + 1] = '(';
+        dance_layer[playerPos + 2] = eyes;
+        dance_layer[playerPos + 3] = '_';
+        dance_layer[playerPos + 4] = eyes;
+        dance_layer[playerPos + 5] = '/';
+        dance_layer[playerPos + 6] = ')';
+      }
+
+      // Once player reaches far end of the base, reverse direction
+      if (playerPos == player_layer.length() - 7 - input_message.length()) {
+        playerDirection = LEFT;
+        loop++;
+      } else if (playerPos == 0) {
+        playerDirection = RIGHT;
+        loop++;
+      }
+
+      if (loop % 10 == 0) {
+        eyes = 'o';
+      } else if (loop % 5 == 0) {
+        eyes = '^';
+      }
+
+      RCLCPP_INFO(this->get_logger(), dance_layer.c_str());
+    }
+
+    void reset()
+    {
+        count_ = 0;
+        curr_msg_pos = 0;
+        player_layer = base_layer;
+        playerPos = 0;
+        playerDirection = RIGHT;
     }
 };
 
