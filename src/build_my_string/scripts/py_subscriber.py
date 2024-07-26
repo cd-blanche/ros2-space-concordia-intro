6ea66c5ee9b62811e62be931eb8eb2ce964abdfd
poewt ubuntu <79345528+cd-blanche@users.noreply.github.com>
2024-07-25 21:09:07 -0400

    Init python subscriber node

---
+++ src/build_my_string/scripts/py_subscriber.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from build_my_string.msg import Message
+
+class MessageSubscriber(Node):
+
+  def __init__(self):
+    super().__init__('message_subscriber')
+    self.subscription = self.create_subscription(
+      Message,
+      'topic',
+      self.listener_callback,
+      10
+    )
+    self.subscription
+
+  def listener_callback(self, msg):
+    self.get_logger().info('hearing: "%s"' % msg.data)
+
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  message_subscriber = MessageSubscriber()
+
+  rclpy.spin(message_subscriber)
+
+  message_subscriber.destroy_node()
+  rclpy.shutdown()
+
+
+if __name__ == '__main__':
+  main()
+
